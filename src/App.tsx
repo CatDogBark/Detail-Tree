ce66f234d37744953cc164a06c00b52bc3eada21
CatDogBark <troyevangelist@gmail.com>
2025-02-07 13:52:27 -1000

    Add family tree component and GitHub Pages configuration

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import FamilyTree from "./components/FamilyTree";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <FamilyTree />
+    </div>
+  );
+}
+
+export default App;
